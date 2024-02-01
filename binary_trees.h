997b460a52f102780c6db0f9b4382f6d6a72a2d8
zaki82000 <yabdelshafy0@gmail.com>
2024-02-01 10:42:24 +0200

    add insert file

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -18,4 +21,6 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
