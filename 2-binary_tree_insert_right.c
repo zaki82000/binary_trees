d8fc3a20a19cfffe626d3fe371c4363dfaf440fb
zaki82000 <yabdelshafy0@gmail.com>
2024-02-02 08:53:53 +0200

    update

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,34 +1,39 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_right- a function that inserts a
- * node as the left-child of another node
- * @parent: is a pointer to the node to insert the left-child in.
- * @value: is the value to store in the new node.
- * Return: return newnode.
- */
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: pointer to the node to insert the right-child in.
+ * @value: value to store in the new node.
+ * Return: pointer to the new node.
+*/
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *NewNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+if (parent == NULL)
+{
+fprintf(stderr, "parent is NULL\n");
+return (NULL);
+}
 
-    if (NewNode == NULL)
-    {
-        fprintf(stderr, "Memory allocation failed\n");
-        return (NULL);
-    }
+binary_tree_t *newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-    NewNode->n = value;
-    NewNode->left = NULL;
-    NewNode->right = NULL;
-    NewNode->parent = parent;
+if (newNode == NULL)
+{
+fprintf(stderr, "Memory allocation failed\n");
+return (NULL);
+}
 
-    if (parent->right != NULL)
-    {
-        NewNode->right = parent->right;
-        NewNode->right->parent = NewNode;
-    }
+newNode->n = value;
+newNode->left = NULL;
+newNode->right = NULL;
+newNode->parent = parent;
 
-    parent->right = NewNode;
+if (parent->right != NULL)
+{
+newNode->right = parent->right;
+newNode->right->parent = newNode;
+}
 
-    return (NewNode);
+parent->right = newNode;
+
+return (newNode);
 }
