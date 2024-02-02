a47e2429cceb4ec089a24b02defc3952d11e7235
zaki82000 <yabdelshafy0@gmail.com>
2024-02-02 08:56:18 +0200

    update

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,9 +11,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 binary_tree_t *newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-if (parent == NULL)
+if (!parent)
 {
-fprintf(stderr, "parent is NULL\n");
 return (NULL);
 }
 
