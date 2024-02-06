e4d9ecc10aad1e9319ad97d192166979c87dbf1a
zaki82000 <yabdelshafy0@gmail.com>
2024-02-06 08:49:39 +0200

    add binary tree delete filr

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -4,15 +4,15 @@
  * binary_tree_delete- a function that deletes an entire binary tree.
  * @tree: is a pointer to the root node of the tree to delete.
  * Return: return NULL if root == NULL.
-*/
+ */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-binary_tree_delete(tree->left);
-binary_tree_delete(tree->right);
+    if (tree == NULL)
+        return;
 
-if (tree == NULL)
-return;
+    binary_tree_delete(tree->left);
+    binary_tree_delete(tree->right);
 
-free(tree);
+    free(tree);
 }
