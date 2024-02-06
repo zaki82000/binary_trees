eb518654c90ffe592fdfeca55c956e2f86dcdcb6
zaki82000 <yabdelshafy0@gmail.com>
2024-02-06 08:44:00 +0200

    add binary tree delete filr

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,11 +8,11 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-if (tree == NULL)
-return;
-
 binary_tree_delete(tree->left);
 binary_tree_delete(tree->right);
+
+if (tree == NULL)
+return;
 
 free(tree);
 }
